c29f51a762c6bf840585abe2878fdb9a5b7ae94c
harinarayan111 <154792817+harinarayan111@users.noreply.github.com>
2023-12-26 12:45:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+let myResume={
+    "basics": {
+      "name": "HARI NARAYAN.P",
+      "email": "harinarayanprakash2003@gmail.com",
+      "phone": 7339204194,
+      "degree": "Msc",
+      "location": {
+        "address": "O.siruvayal road,kalanivasal,karaikudi",
+        "postalCode": 630002,
+        "city": "Karaikudi",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/harinarayan111"
+        }
+      ]
+    },
+    
+"education": [
+      {
+        "institution": "The American College",
+        "department": "Computer science",
+        "studyType": "fulltime",
+        "batch start year": 2020,
+        "batch end year": 2024,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "html,javascript,css",
+        "level": "beginer",
+      }    
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "codings,writtings",
+      }
+    ]
+  }
+  console.log(myResume);
+  
+
+
